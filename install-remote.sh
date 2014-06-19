cc0229a6c460eee812c027f7b04ace5a79385b71
neochrome <johan@stenqvist.net>
2014-06-19 15:00:34 +0200

    removed some scripts

--- install-remote.sh
+++
@@ -1,1 +0,0 @@
-scp -r install.sh tcmonitor.service package.json server.js public/ root@tcstatus:/srv/tcmonitor/
--- install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cp tcmonitor.service /etc/systemd/system/
-systemctl enable tcmonitor.service
